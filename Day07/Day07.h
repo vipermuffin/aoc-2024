d59c4917b465904b8f3e052262fc8e3bbc6afb77
Viper Muffin <vipermuffin@gmail.com>
2024-12-07 10:25:36 -0600

    Day 7 initial solution

---
+++ Day07/Day07.h
@@ -0,0 +1,18 @@
+//
+//  Advent of Code 2024 Solutions: Day 7
+//
+//  https://adventofcode.com/2024
+//
+//  Created by vipermuffin on 12/06/2024.
+//  Copyright © 2024 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay07 {
+//Function Definitions
+bool isValidEquation(const std::string& input);
+bool isValidEquation2(const std::string& input);
+std::vector<std::string> generatePermutations(const std::string& line);
+}
